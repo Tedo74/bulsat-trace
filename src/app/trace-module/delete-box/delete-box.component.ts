4e43547a45a20b74f0dc56c94659165a133d8b81
Tedo <teddy2@abv.bg>
2020-04-17 01:07:50 +0300

    css edit

--- src/app/trace-module/delete-box/delete-box.component.ts
+++ src/app/trace-module/delete-box/delete-box.component.ts
@@ -1,33 +1,28 @@
 import { Component, OnInit, Input, OnDestroy } from '@angular/core';
-import { BoxModel } from '../box-model';
 import { BoxServService } from '../box-serv.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { TracesServService } from '../traces-serv.service';
-import { Subscription } from 'rxjs';
 
 @Component({
 	selector: 'app-delete-box',
 	templateUrl: './delete-box.component.html',
-	styleUrls: ['./delete-box.component.css']
+	styleUrls: [ './delete-box.component.css' ]
 })
 export class DeleteBoxComponent implements OnInit {
-
 	constructor(
 		private boxServ: BoxServService,
 		private traseServ: TracesServService,
 		private route: ActivatedRoute,
 		private router: Router
-	) { }
+	) {}
 
-	ngOnInit(): void {
-	}
+	ngOnInit(): void {}
 	delete() {
 		this.boxServ.deleteBox();
 		this.traseServ.getTrace(this.boxServ.pathToCollection);
-		this.router.navigate(['../../'], { relativeTo: this.route });
+		this.router.navigate([ '../../' ], { relativeTo: this.route });
 	}
 	cancel() {
-		this.router.navigate(['../../'], { relativeTo: this.route });
+		this.router.navigate([ '../../' ], { relativeTo: this.route });
 	}
-
 }
